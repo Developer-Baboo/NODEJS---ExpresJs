77898fe715f1140a23775a63f41f41acea126a85
Developer Baboo <babooheerani@gmail.com>
2023-10-31 18:23:18 +0500

    practice

---
+++ repl.js
@@ -0,0 +1,10 @@
+// Read Evolute Print Loop
+
+const repl = require("repl");
+const local = repl.start("$");
+
+
+local.on("exit", () => {
+    console.log("exiting REPL");
+    process.exit();
+});
